c77152c228bb14e289ce69f6d9e37e9c6cefbf75
bndkxbqf2g-stack <bndkxbqf2g@privaterelay.appleid.com>
2026-09-22 13:35:38 +0200

    Use custom prices in market value evaluation

--- lib/features/store/store_value.dart
+++ lib/features/store/store_value.dart
@@ -1,4 +1,5 @@
 import '../../models/list_item.dart';
+import '../../models/market_price.dart';
 import '../../models/offer.dart';
 import '../../models/store.dart';
 import '../route/route_optimizer.dart';
@@ -28,18 +29,21 @@ StoreValue evaluateStoreValue(
   DateTime? now,
   Map<String, double>? roadDistances,
   double euroPerKm = 0.22,
+  List<MarketPrice> marketPrices = const <MarketPrice>[],
 }) {
   final summary = buildStoreShoppingSummary(
     store,
     items,
     offers,
     now: now,
+    marketPrices: marketPrices,
   );
   final optimizer = RouteOptimizer(
     items,
     offers,
     roadDistances: roadDistances,
     euroPerKm: euroPerKm,
+    marketPrices: marketPrices,
   );
   final travel = optimizer.travelCost([store]);
   final net = summary.savings - travel;
