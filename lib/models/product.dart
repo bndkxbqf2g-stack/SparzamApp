73b04d6229f9eba0c6b89b4ebe8d1f2c195a8e32
bndkxbqf2g-stack <bndkxbqf2g@privaterelay.appleid.com>
2026-09-22 13:32:01 +0200

    Make products serializable and editable

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -7,6 +7,7 @@ class Product {
     this.aliases = const [],
     this.isFavorite = false,
     this.ean,
+    this.brand,
   });
 
   final String id;
@@ -16,4 +17,51 @@ class Product {
   final List<String> aliases;
   final bool isFavorite;
   final String? ean;
+  final String? brand;
+
+  Product copyWith({
+    String? id,
+    String? name,
+    String? unit,
+    String? group,
+    List<String>? aliases,
+    bool? isFavorite,
+    String? ean,
+    String? brand,
+  }) =>
+      Product(
+        id: id ?? this.id,
+        name: name ?? this.name,
+        unit: unit ?? this.unit,
+        group: group ?? this.group,
+        aliases: aliases ?? this.aliases,
+        isFavorite: isFavorite ?? this.isFavorite,
+        ean: ean ?? this.ean,
+        brand: brand ?? this.brand,
+      );
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'name': name,
+        'unit': unit,
+        'group': group,
+        'aliases': aliases,
+        'isFavorite': isFavorite,
+        'ean': ean,
+        'brand': brand,
+      };
+
+  factory Product.fromJson(Map<String, dynamic> json) => Product(
+        id: json['id'] as String,
+        name: json['name'] as String,
+        unit: json['unit'] as String,
+        group: json['group'] as String,
+        aliases: (json['aliases'] as List<dynamic>?)
+                ?.whereType<String>()
+                .toList() ??
+            const <String>[],
+        isFavorite: json['isFavorite'] as bool? ?? false,
+        ean: json['ean'] as String?,
+        brand: json['brand'] as String?,
+      );
 }
