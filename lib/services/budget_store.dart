11be6d31d87610cac2da23ddb2b1c4804bd91807
bndkxbqf2g-stack <bndkxbqf2g@privaterelay.appleid.com>
2026-09-23 07:26:55 +0200

    Remove unnecessary budget test platform setup

--- lib/services/budget_store.dart
+++ lib/services/budget_store.dart
@@ -23,21 +23,9 @@ class BudgetStore {
       throw ArgumentError('Budgetbeträge müssen endlich und nichtnegativ sein.');
     }
     await Future.wait([
-      _preferences.setDouble(
-        _monthlyKey,
-        plan.monthlyBudget,
-        const SharedPreferencesOptions(),
-      ),
-      _preferences.setDouble(
-        _foodKey,
-        plan.foodBudget,
-        const SharedPreferencesOptions(),
-      ),
-      _preferences.setDouble(
-        _spentKey,
-        plan.foodSpent,
-        const SharedPreferencesOptions(),
-      ),
+      _preferences.setDouble(_monthlyKey, plan.monthlyBudget),
+      _preferences.setDouble(_foodKey, plan.foodBudget),
+      _preferences.setDouble(_spentKey, plan.foodSpent),
     ]);
   }
 }
