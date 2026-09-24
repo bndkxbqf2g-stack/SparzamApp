9302ad7cfe1c3bb2ceca09258e2ad7637192972c
bndkxbqf2g-stack <bndkxbqf2g@privaterelay.appleid.com>
2026-09-24 09:17:05 +0200

    Add receipt_observation.dart for receipt observation learning

---
+++ lib/models/receipt_observation.dart
@@ -0,0 +1,64 @@
+class ReceiptObservation {
+  const ReceiptObservation({
+    required this.id,
+    required this.receiptFingerprint,
+    required this.rowLine,
+    required this.rawLabel,
+    required this.familyKey,
+    required this.storeName,
+    required this.observedAt,
+    required this.totalPrice,
+    required this.quantity,
+    required this.quantityUnit,
+    required this.unitPrice,
+    required this.discounted,
+    this.productId,
+  });
+
+  final String id;
+  final String receiptFingerprint;
+  final int rowLine;
+  final String rawLabel;
+  final String familyKey;
+  final String storeName;
+  final DateTime observedAt;
+  final double totalPrice;
+  final num? quantity;
+  final String quantityUnit;
+  final double? unitPrice;
+  final bool discounted;
+  final String? productId;
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'receiptFingerprint': receiptFingerprint,
+        'rowLine': rowLine,
+        'rawLabel': rawLabel,
+        'familyKey': familyKey,
+        'storeName': storeName,
+        'observedAt': observedAt.toIso8601String(),
+        'totalPrice': totalPrice,
+        'quantity': quantity,
+        'quantityUnit': quantityUnit,
+        'unitPrice': unitPrice,
+        'discounted': discounted,
+        'productId': productId,
+      };
+
+  factory ReceiptObservation.fromJson(Map<String, dynamic> json) =>
+      ReceiptObservation(
+        id: json['id'] as String,
+        receiptFingerprint: json['receiptFingerprint'] as String,
+        rowLine: (json['rowLine'] as num).toInt(),
+        rawLabel: json['rawLabel'] as String,
+        familyKey: json['familyKey'] as String? ?? '',
+        storeName: json['storeName'] as String,
+        observedAt: DateTime.parse(json['observedAt'] as String),
+        totalPrice: (json['totalPrice'] as num).toDouble(),
+        quantity: json['quantity'] as num?,
+        quantityUnit: json['quantityUnit'] as String? ?? 'Stück',
+        unitPrice: (json['unitPrice'] as num?)?.toDouble(),
+        discounted: json['discounted'] as bool? ?? false,
+        productId: json['productId'] as String?,
+      );
+}
