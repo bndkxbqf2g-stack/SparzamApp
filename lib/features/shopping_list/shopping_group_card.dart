bb5b5fa96650f0736f23a95e30b840aeb0bf2908
bndkxbqf2g-stack <bndkxbqf2g@privaterelay.appleid.com>
2026-09-25 09:48:39 +0200

    fix: keep butter in dairy shopping group

--- lib/features/shopping_list/shopping_group_card.dart
+++ lib/features/shopping_list/shopping_group_card.dart
@@ -319,7 +319,7 @@ class _ShoppingItemTile extends StatelessWidget {
 }
 
 String shoppingGroupLabel(String group) => switch (group) {
-      'milch' => 'Milch & Käse',
+      'milch' || 'butter' => 'Milch & Käse',
       'obst' => 'Obst & Gemüse',
       'fleisch' => 'Fleisch',
       'nudeln' => 'Nudeln & Beilagen',
