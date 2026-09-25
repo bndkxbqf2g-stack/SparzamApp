fe593d1a2f10df155e3ffd32938216660f3635b9
bndkxbqf2g-stack <bndkxbqf2g@privaterelay.appleid.com>
2026-09-25 15:52:56 +0200

    test: initialize preferences for receipt import widget

--- test/receipt_import_dialog_test.dart
+++ test/receipt_import_dialog_test.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences_platform_interface/in_memory_shared_preferences_async.dart';
+import 'package:shared_preferences_platform_interface/shared_preferences_async_platform_interface.dart';
 import 'package:sparzamapp/features/receipt/receipt_import_dialog.dart';
 
 void main() {
+  setUp(() {
+    SharedPreferencesAsyncPlatform.instance =
+        InMemorySharedPreferencesAsync.empty();
+  });
+
+  tearDown(() {
+    SharedPreferencesAsyncPlatform.instance = null;
+  });
+
   testWidgets('receipt import uses clear Bonpreise action label', (tester) async {
     await tester.pumpWidget(
       MaterialApp(
