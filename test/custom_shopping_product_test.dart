7e78ff9ffd771354840295c2a5b69758abef7613
bndkxbqf2g-stack <bndkxbqf2g@privaterelay.appleid.com>
2026-09-25 12:28:27 +0200

    test: preserve identities for free shopping input

--- test/custom_shopping_product_test.dart
+++ test/custom_shopping_product_test.dart
@@ -10,6 +10,12 @@ void main() {
     expect(product.group, 'custom');
   });
 
+  test('bekannte freie Eingabe behält ihre Produktfamilie', () {
+    expect(customShoppingProduct('Schmand').group, 'schmand');
+    expect(customShoppingProduct('Tomate').group, 'tomaten');
+    expect(customShoppingProduct('Tomatenmark').group, 'tomatenmark');
+  });
+
   test('Sonderzeichen verwenden sichere Ersatz-ID', () {
     expect(customShoppingProduct('ÄÖÜ').id, 'custom_artikel');
   });
