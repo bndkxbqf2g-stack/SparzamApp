85464cf4328a947c0e3dbb92a59307c9df49a73d
bndkxbqf2g-stack <bndkxbqf2g@privaterelay.appleid.com>
2026-09-26 15:58:35 +0200

    test: scroll prospect tile into viewport before tapping

--- test/prospects_screen_test.dart
+++ test/prospects_screen_test.dart
@@ -92,7 +92,7 @@ void main() {
     expect(find.text('2,39 €'), findsOneWidget);
     expect(find.text('2,99 €'), findsOneWidget);
 
-    await tester.ensureVisible(find.text('2,39 €'));
+    await tester.scrollUntilVisible(find.text('2,39 €'), 300);
     await tester.tap(find.text('2,39 €'));
     await tester.pump();
     expect(added, isTrue);
